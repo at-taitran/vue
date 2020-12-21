65c1a6b7501d25f705f489135ae492cf7da5fef5
MBA0055 <mba0055@MBA0055.local>
2020-12-21 13:49:38 +0700

    create

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,120 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <form>
+      <div class="container">
+        <h2>Select an image</h2>
+        <input type="file" id="files" multiple="multiple" @change="onFileChange">
+      </div>
+      <div class="container wrap" v-if="images">
+        <div class="b-img" v-for="(i, index) in images" :key="index">
+          <img :src="i"/>
+          <button @click="removeImage(index)">Remove image</button>
+        </div>
+      </div>
+      <div class="container">
+        <button v-on:click="submitFile()" type="submit">Upload</button>
+      </div>
+    </form>  
   </div>
-</template>
-
+</template> 
 <script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      imageData: null,
+      images: [],
+      currentFile:[undefined]
+    }
+  },
+  methods: {
+    onFileChange(e) {
+      const files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files);
+    },
+    createImage(files) {
+      const vm = this;
+      for (var index = 0; index < files.length; index++) {
+        let reader = new FileReader();
+        reader.onload = function(event) {
+          const imageUrl = event.target.result;
+           vm.images.push(imageUrl);
+        } 
+        reader.readAsDataURL(files[index]);
+      }
+    },
+    removeImage(index) {
+      this.images.splice(index.splice(index), 1)
+      console.log(this.images[index ])
+    },
+    // submitFile(e) {
+    //   let formData = new FormData();
+    //   let v = this;
+    //   const files = e.target.files || e.dataTransfer.files; 
+    //   for (let index = 0; index < files.length; index++) {
+    //     let file = v.files[index];
+    //     formData.append('files[' + index + ']', file);
+    //   }
+    //   axios.post('/select-files',
+    //   formData,{
+    //     header:{
+    //       'Content-Type': 'multipart/form-data'
+    //     }
+    //   }
+    //   ).then(function() {
+    //     console.log('SUCCESS!!');
+    //   }).catch(function() {
+    //     console.log('FAIL!!');
+    //     console.log(formData)
+    //   })
+    //   // ,
+    //   // this.handleFileUpload();
+    // },
+    // handleFileUpload() {
+    //   let uploadFiles = this.$refs.files.files;
+    //   for (let index = 0; index < uploadFiles.length; index++) {
+    //     this.files.push(uploadFiles[index]);
+    //   }
+    //   console.log(uploadFiles)
+    // }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container{
+  width: 1200px;
+  margin-left: auto;
+  margin-right:auto;
+  padding: 15px
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.show{
+  width: auto;
+  height: 100px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+img {
+  width: 200px;
+  height: 75px;
+  margin: auto;
+  display: block;
+  margin-bottom: 10px;
+  object-fit: cover;
 }
-a {
-  color: #42b983;
+.b-img{
+  padding: 1rem;
+}
+.wrap{
+  width: 100%;
+  max-width: 1200px;
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
 }
 </style>
